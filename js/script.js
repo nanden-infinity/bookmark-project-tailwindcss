f36f48549f59e6674140166f7c8737e1a5453850
Inanden <147476553+Inanden@users.noreply.github.com>
2025-01-23 03:37:06 +0000

    ultimo commint com todas as section incl e funcionalidades de js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 const links = document.querySelectorAll(".nav-link");
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btnMenu = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+const btnLogin = document.getElementById("goToLogin");
 
+function initFunction() {
 function onTabClick(event) {
   // Desactivate All Tabs
   tabs.forEach((tab) => {
@@ -12,7 +17,7 @@ function onTabClick(event) {
       "md:border-b-0"
     );
   });
-// document.querySelector('.tab')?.classList.remove("border-softRed", "border-b-4", "md:border-b-0");
+  // document.querySelector('.tab')?.classList.remove("border-softRed", "border-b-4", "md:border-b-0");
   // Hide All Panels
   panels.forEach((panel) => panel.classList.add("hidden"));
 
@@ -20,11 +25,52 @@ function onTabClick(event) {
 
   event.target.classList.add("border-softRed", "border-b-4");
   const classString = event.target.getAttribute("data-target");
- 
-    document
-      .getElementById("panels")
-      .getElementsByClassName(classString)[0]
-      .classList.remove("hidden")
+
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
 }
 
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+function navToggle() {
+  this.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
+const linkMenu = document.querySelectorAll(".menu--link");
+
+function goToPage(event) {
+  event.preventDefault();
+  menu.classList.add("hidden");
+  menu.classList.remove("flex");
+  btnMenu.classList.remove("open");
+  logo.setAttribute("src", "./images/logo-bookmark.svg");
+  const href = event.target.getAttribute("href");
+  const link = event.target;
+
+  if (href.includes("#")) {
+    const id = href.split("#")[1];
+    console.log(id);
+    const section = document.getElementById(id);
+    section.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
+}
+
+
+linkMenu.forEach((link) => link.addEventListener("click", goToPage));
+
+btnMenu.onclick = navToggle;
+
+
+}
+
+
+initFunction()
