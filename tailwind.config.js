9a6dd17b99fa811a750014e7f3ef3f0efee6907b
Inanden <147476553+Inanden@users.noreply.github.com>
2025-01-27 11:01:08 +0000

    testando tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,18 +9,8 @@ module.exports = {
       xl: "1440px",
     },
     extend: {
-      colors: {
-        softBlue: "hsl(231, 69%, 60%)",
-        softRed: "hsl(0, 94%, 66%)",
-        grayishBlue: "hsl(229, 8%, 60%)",
-        veryDarkBlue: "hsl(229, 31%, 21%)",
-      },
-      fontFamily: {
-        sans: ["Rubik", "sans-serif"],
-      },
-      backgroundImage: () => ({
-        dots: "url('../images/bg-dots.svg')",
-      }),
+      colors: {},
+      fontFamily: {  }
     },
   },
   plugins: [],
