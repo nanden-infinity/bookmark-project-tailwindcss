4e40402581f103407c51df4dacade0beca239df6
Inanden <147476553+Inanden@users.noreply.github.com>
2025-01-27 11:42:10 +0000

    ultimo commint

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,18 @@ module.exports = {
       xl: "1440px",
     },
     extend: {
-      colors: {},
-      fontFamily: {  }
+      colors: {
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')",
+      }),
     },
   },
   plugins: [],
